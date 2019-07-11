25db0730e22445cecd63528a3a4dc966c8a6499c
sanvean74 <dlambertpdx@gmail.com>
2019-07-11 11:32:17 -0700

    working madlib with css styling

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-let person1 = document.getElementById('person-name-1');
-let person2 = document.getElementById('person-name-2');
-let verb1 = document.getElementById('verb-1');
-let verb2 = document.getElementById('verb-2');
-let verb3 = document.getElementById('verb-3');
-let animal1 = document.getElementById('animal-1');
-let adverb1 = document.getElementById('adverb-1');
-
+let person1Input = document.getElementById('person-name-1');
+let person2Input = document.getElementById('person-name-2');
+let verb1Input = document.getElementById('verb-1');
+let verb2Input = document.getElementById('verb-2');
+let verb3Input = document.getElementById('verb-3');
+let animal1Input = document.getElementById('animal-1');
+let adverb1Input = document.getElementById('adverb-1');
 
 function revealMadlib() {
-    person1.textContent = person1.value;
-    console.log('Tada!');
-}
+    let person1 = person1Input.value;
+    let person2 = person2Input.value;
+    let verb1 = verb1Input.value;
+    let verb2 = verb2Input.value;
+    let verb3 = verb3Input.value;
+    let animal1 = animal1Input.value;
+    let adverb1 = adverb1Input.value;
 
-revealMadlib();
+    let fillInMadlib =
+        person1 +
+        ' and ' +
+        person2 +
+        ' were ' +
+        verb1 +
+        ' through the woods when suddenly a ferocious ' +
+        animal1 +
+        ' appeared! ' +
+        adverb1 +
+        ', they ' +
+        verb2 +
+        ' to a nearby tree, where they ' +
+        verb3 +
+        ' until morning.';
+    let displayStory = document.getElementById('story');
+    displayStory.textContent = fillInMadlib;
+}
